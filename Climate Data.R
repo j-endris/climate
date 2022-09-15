9058b610bb9d5530bff9133fd1cbfdbe5549080f
Joe Endris <joseph.endris@outlook.com>
2022-09-15 15:58:25 -0500

    climate data set-up and initial julian date work.

--- Climate Data.R
+++ Climate Data.R
@@ -3,17 +3,38 @@
 
 library(readr)
 library(dplyr)
+library(tidyverse)
 library(ggplot2)
-library(ggfortify)
-library(emmeans)
-library(multcomp)
-library(multcompView)
-library(HH)
+library(lubridate)
 
 ## create objects from datasets##
 
-MI <- read_csv("Michigan.csv" )
+MI <- read_csv("Michigan.csv")
 IN <- read_csv("Alabama.csv")
 AL <- read_csv("Alabama.csv")
+TN <- read_csv("Tennessee.csv")
 
-mutate
+str(AL)  #view structure of data ##
+
+## create column for julian date##
+## trying to replicate https://stackoverflow.com/questions/21414847/convert-a-date-vector-into-julian-day-in-r##
+AL_julian <- mutate(AL, Julian=format("%j"))
+
+
+## monthly mean low temp ##
+## update this after creating julian dates ##
+AL %>%
+  group_by(month) %>%
+  summarise(total = mean(value))
+
+## create graph for temps by month of year ##
+
+AL %>%
+  ggplot(aes(x = DATE, y = TMIN)) +
+  geom_point(color = "grey") +
+  labs(title = "Daily Low Temperatures",
+       subtitle = "Tuscaloosa, AL",
+       y= "Daily Low Temperature (Celcius)",
+       x= "Date") + theme_bw(base_size = 15)
+
+  
