7f16157dd67e8de51d8e0b6a4c1c18306d3a42a8
Joe Endris <joseph.endris@outlook.com>
2022-12-19 18:15:47 -0600

    more updates

--- Climate Data (cold).R
+++ Climate Data (cold).R
@@ -30,25 +30,25 @@ AL<-AL[complete.cases(AL[,5]),]
 
 #calculate last day below freezing for each year
 AL_last <- AL%>%
-  dplyr::group_by(year=lubridate::floor_date(DATE, "day")) %>%
-  dplyr::last (TMIN, 0)
+  group_by(julian_date) %>%
+  dplyr::last (AL$TMIN, 0)
 
 ## monthly absolute low temp ##
-## update this after creating julian dates ##
 AL_TMIN <- AL %>%
   group_by(month=lubridate::floor_date(DATE, "month")) %>%
   summarise(temp = min(TMIN))
 
 ## create graph for temps by month of year ##
-AL_TMIN %>%
-  filter(year>1980) %>%
-  ggplot(aes(x = year, y = total)) +
+AL_TMIN_plot <-
+  ggplot(AL_TMIN, aes(x = month, y = temp)) +
   geom_point(color = "black") +
   geom_smooth(stat="smooth",method="lm")+
   labs(title = "Annual Lowest Temperatures",
        subtitle = "Tuscaloosa, AL",
        y= "Temperature (Celcius)",
        x= "Year") + theme_bw(base_size = 15)
+
+AL_TMIN_plot
 
 #Number of Days Below -2
 AL_freeze <- AL %>%
@@ -90,118 +90,41 @@ AL_mean_plot <- ggplot(AL_mean, aes(x= julian_date, y=mean_low))+
                               x= "Julian Date") + theme_bw(base_size = 15)
 
 AL_mean_plot
-##################################
-####Tennessee segment starts here###
-##################################
 
-str(TN)  #view structure of data ##
 
-## create column for julian date##
-TN <- mutate(TN, julian_date=format(DATE,"%j"))
+## plot monthly mean low temps for Jan-May ##
 
-#omit NA in temperature recordings 
-TN<-TN[complete.cases(TN[,5]),]
+AL_month_mean <- AL %>%
+  group_by(month=lubridate::floor_date(DATE, "month")) %>%
+  summarise(mean_low = mean(TMIN))
 
-#calculate last day below freezing for each year
-TN_last <- TN %>%
-  group_by(year=lubridate::floor_date(DATE, "year")) %>%
-  mutate(last = day(min(which(TMIN<=0))))
-  
-## monthly mean low temp ##
-## update this after creating julian dates ##
-TN_TMIN <- TN %>%
-  group_by(year=lubridate::floor_date(DATE, "year")) %>%
-  summarise(total = min(TMIN))
-
-## create graph for temps by month of year ##
-TN_TMIN %>%
-  filter(year>1980-01-01) %>%
-  ggplot(aes(x = year, y = total)) +
-  geom_point(color = "grey") +
-  geom_smooth(stat="smooth",method="lm")+
-  labs(title = "Annual Lowest Temperatures",
-       subtitle = "Clarksville, TN",
-       y= "Daily High Temperature (Celcius)",
-       x= "Year") + theme_bw(base_size = 15)
-
-TN_TMIN %>%
-  filter(year>"1940")
-
-#Number of days below zero
-TN_freeze <- TN %>%
-  group_by(year=lubridate::floor_date(DATE, "year")) %>%
-  summarise(n=sum(TMIN<0))
-
-#plot number of days below zero
-TN_freeze %>%
-  filter(as.integer(year)>1980)%>%
-  filter(n>0)%>%
-  ggplot(aes(x = year, y = n)) +
+AL_monthly_mean_plot <- ggplot(AL_month_mean, aes(x= month, y=mean_low))+
   geom_point(color = "grey") +
   geom_smooth(method="lm")+
-  labs(title = "Number of Days Below Freezing",
-       subtitle = "Clarksville, TN",
-       y= "Number of Days",
-       x= "Year") + theme_bw(base_size = 15)
+  labs(title = "Monthly Mean Low Temperture",
+       subtitle = "Tuscaloosa, AL",
+       y= "Temperature (C)",
+       x= "Month") + theme_bw(base_size = 15)
+
+AL_monthly_mean_plot
 
 
-TN_freeze %>%
-  filter(n>0)%>%
-  filter(year>1980)
+
+
+
+
+
+####################################
+####Tennessee segment starts here###
+####################################
+
+
 
 ##################################
 ####Indiana segment starts here###
 ##################################
 
-str(IN)  #view structure of data ##
 
-## create column for julian date##
-## trying to replicate https://stackoverflow.com/questions/21414847/convert-a-date-vector-into-julian-day-in-r##
-IN <- mutate(IN, julian_date=format(DATE,"%j"))
-
-#omit NA in temperature recordings 
-IN<-IN[complete.cases(IN[,5]),]
-
-## monthly mean low temp ##
-## update this after creating julian dates ##
-IN_TMIN <- IN %>%
-  group_by(year=lubridate::floor_date(DATE, "year")) %>%
-  summarise(total = min(TMIN))
-
-## create graph for temps by month of year ##
-
-IN_TMIN %>%
-  filter(year>1980) %>%
-  ggplot(aes(x = year, y = total)) +
-  geom_point(color = "grey") +
-  geom_smooth(stat="smooth",method="lm")+
-  labs(title = "Annual Lowest Temperatures",
-       subtitle = "Hoosier National Forest, IN",
-       y= "Daily High Temperature (Celcius)",
-       x= "Year") + theme_bw(base_size = 15)
-
-
-#number of days below zero
-IN_freeze <- IN %>%
-  group_by(year=lubridate::floor_date(DATE, "year")) %>%
-  summarise(n=sum(TMIN<0))
-
-#plot Number of Days Below -2
-IN_freeze %>%
-  filter(as.integer(year)>1980)%>%
-  filter(n>0)%>%
-  ggplot(aes(x = year, y = n)) +
-  geom_point(color = "grey") +
-  geom_smooth(method="lm")+
-  labs(title = "Number of Days Below -2C",
-       subtitle = "Hoosier National Forest, IN",
-       y= "Number of Days",
-       x= "Year") + theme_bw(base_size = 15)
-
-
-IN_freeze %>%
-  filter(n>0)%>%
-  filter(year>1980)
 
 #Issues to correct
 #how to account for/correct leap days in Julian date count
